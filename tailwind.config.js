7c392e5f196c48c8ae3c12f3ab52c56e10e6b8a7
David Belleau <david-belleau@hotmail.com>
2023-05-04 15:45:21 +0200

    feat/config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit', // use arbitrary CSS statements
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'colorSearchIcon' : '#8de8fe'
+      },
+      fontSize: {
+        'sizeIcon': '2rem',
+      },
+    },
+    screens:{
+      'sm': {
+        'min':'550px'
+      }
+    }
   },
   plugins: [],
 }
